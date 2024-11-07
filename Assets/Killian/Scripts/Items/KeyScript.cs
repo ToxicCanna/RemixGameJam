142820bfc1f27c913438e4d4ef2342445674a5de
ToxicCanna <freedom22140@gmail.com>
2024-11-06 18:37:45 -0700

    Minor patch

--- Assets/Killian/Scripts/Items/KeyScript.cs
+++ Assets/Killian/Scripts/Items/KeyScript.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
-public class KeyScript : MonoBehaviour
+public class KeyScript : MonoBehaviour, ICollect
 {
+    public void Collect()
+    {
+        
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
