50cfbb93a01aeba3989b22005f8f5181023325ac
Nibour16 <ruobinh3@hotmail.com>
2024-11-07 19:37:32 -0700

    Add Key Shop, before this is a power-up shop

--- Assets/Robin/ShopManager.cs
+++ Assets/Robin/ShopManager.cs
@@ -3,26 +3,47 @@ using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    [Header("Cost")]
-    [SerializeField] public int cost;
-    [SerializeField] private int costIncrement;
-    [SerializeField] private Text costText;
-
+    #region SerializedVariables
     [Header("Reference")]
     [SerializeField] PlayerStats playerStats;
     [SerializeField] PlayerHealth player1Health;
     [SerializeField] PlayerHealth player2Health;
-    [SerializeField] GameObject Shop;
+    [SerializeField] GameObject powerUpShop;
+    [SerializeField] GameObject keyShop;
+
+    [Header("Text")]
+    [SerializeField] private Text StatCostText;
+    [SerializeField] private Text YKeyCostText;
+    [SerializeField] private Text BKeyCostText;
+
+    [Header("StatCost")]
+    [SerializeField] public int cost;
+    [SerializeField] private int costIncrement;
+    [SerializeField] public int YKeyCost;
+    [SerializeField] public int BKeyCost;
 
     [Header("IncrementValue")]
     [SerializeField] private int HPIncrement;
     [SerializeField] private int ATKIncrement;
     [SerializeField] private int DEFIncrement;
+    #endregion
+
+    #region Initialization
+    private void Start()
+    {
+        powerUpShop.SetActive(false);
+        keyShop.SetActive(false);
+    }
 
     private void Update()
     {
-        costText.text = "Cost: $" + cost.ToString();
+        StatCostText.text = "Cost: $" + cost.ToString();
+        YKeyCostText.text= "YKey ($" + YKeyCost.ToString() + ")";
+        BKeyCostText.text = "BKey ($" + BKeyCost.ToString() + ")";
     }
+    #endregion
+
+    #region StatShop
     public void PurchaseHP()
     {
         if (cost <= playerStats.gold)
@@ -68,9 +89,47 @@ public class ShopManager : MonoBehaviour
             Debug.Log("Sorry, you don't have enough gold");
         }
     }
+    #endregion
 
-    public void ExitShop()
+    #region KeyShop
+    public void PurchaseYKey()
     {
-        Shop.SetActive(false);
+        if (YKeyCost <= playerStats.gold)
+        {
+            playerStats.PayGold(YKeyCost);
+            playerStats.YKeys++;
+            Debug.Log("You have " + playerStats.YKeys + " yellow keys");
+        }
+        else
+        {
+            Debug.Log("Sorry, you don't have enough gold");
+        }
     }
+
+    public void PurchaseBKey()
+    {
+        if (BKeyCost <= playerStats.gold)
+        {
+            playerStats.PayGold(BKeyCost);
+            playerStats.BKeys++;
+            Debug.Log("You have " + playerStats.BKeys + " blue keys");
+        }
+        else
+        {
+            Debug.Log("Sorry, you don't have enough gold");
+        }
+    }
+    #endregion
+
+    #region ExitShop
+    public void ExitPowerUpShop()
+    {
+        powerUpShop.SetActive(false);
+    }
+
+    public void ExitKeyShop()
+    {
+        keyShop.SetActive(false);
+    }
+    #endregion
 }
